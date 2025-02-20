00b14a00efa5de934232fbdf2eb1ca24bfbb80ae
pedro.almeida <pedro.almeida@rtcom.pt>
2025-02-20 16:36:04 +0000

    Client component efficient

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,28 +4,29 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png"
 import classes from "./main-header.module.css"
 import MainHeaderBackground from "./main-header-background";
- 
-export default function MianHeader(){
-    return(
-        <>
-        <MainHeaderBackground/>
-        <header className={classes.header}>
-            <Link className={classes.logo} href="/">
-            <Image src={logoImg} alt="A plate with food on it" />
-            NextLevel Food
-            </Link>
+import NavLink from "./nav-link";
 
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href="/meals">Browse Meals</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Food Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+export default function MianHeader() {
+    return (
+        <>
+            <MainHeaderBackground />
+            <header className={classes.header}>
+                <Link className={classes.logo} href="/">
+                    <Image src={logoImg} alt="A plate with food on it" />
+                    NextLevel Food
+                </Link>
+
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                        <NavLink href="/meals">Browse Meals</NavLink>
+                        </li>
+                            <li>
+                            <NavLink href="/community">Food Community</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         </>
     );
 
